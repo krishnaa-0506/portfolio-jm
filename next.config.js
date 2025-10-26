7774ed5abebda2a40fde729e9ff760a080cb822a
Krish <you@example.com>
2025-10-26 23:09:55 +0530

    jm

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['react-parallax-tilt'],
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
